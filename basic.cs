7dfe40fc7960276bc84d6623e21a0265af3d1d31
Drich <drichie_boi@yahoo.com>
2020-03-07 08:03:38 +0800

    login user test update

--- basic.cs
+++ basic.cs
@@ -5,6 +5,7 @@ using System.Threading;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support;
 
 namespace NUnitTestProject1
 {
@@ -22,13 +23,16 @@ namespace NUnitTestProject1
         public void UserSignIn()
         {
             driver.Navigate().GoToUrl("https://cisdev.racami.com/CISDEV7/login/acompany");
+            driver.Manage().Window.Maximize();
             driver.FindElement(By.Id("inputUsername")).SendKeys("drichie");
             driver.FindElement(By.Id("inputPassword")).SendKeys("Testing!123"); 
             driver.FindElement(By.Id("btnSignIn")).Click();
             Thread.Sleep(5000);
             driver.FindElement(By.CssSelector("input[type='text']")).SendKeys("Acompany");
-            driver.FindElement(By.XPath("//span[contains(text(), 'Search')]]")).Click();
-            driver.FindElement(By.CssSelector("span[innerText='Go']")).Click();
+            //driver.FindElement(By.XPath("//span[text()='Acompany']")).Click();
+            //IList<IWebElement> elements = driver.FindElements(By.XPath("//*[text()='Search']"));
+            //elements[0].Click();
+            driver.FindElement(By.Id("GoBtn")).Click();
             Thread.Sleep(5000);
         }
 
